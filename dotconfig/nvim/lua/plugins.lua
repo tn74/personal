861527f396c28a654814955b14c0d4ee7b598e65
Trishul Nagenalli <trishul.nagenalli@gmail.com>
2026-06-21 12:55:06 -0400

    unpin treesitter

--- dotconfig/nvim/lua/plugins.lua
+++ dotconfig/nvim/lua/plugins.lua
@@ -104,7 +104,7 @@ require('packer').startup(function(use)
 
   -- Telescope and Plugins
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    'nvim-telescope/telescope.nvim', tag = '*',
     requires = {
       { 'nvim-lua/plenary.nvim' },
       { "nvim-telescope/telescope-live-grep-args.nvim" },
